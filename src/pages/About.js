2aaeb706bb0e9c75171411cb2755646c3bb3ba63
unknown <kacprosh@icloud.com>
2025-01-13 14:40:33 +0100

    about page WIP

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/About.css'
+
+function About() {
+  return (
+    <div className="About">
+      <h1>niga</h1>
+    </div>
+  )
+}
+
+export default About
